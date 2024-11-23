34a9145be1a1ee06b3ddfa69e5de222e4fbf2556
Walfask <walfask@gmail.com>
2024-11-22 22:00:25 -0500

    Revert to discord 1.7.3

--- src/cat_bot.py
+++ src/cat_bot.py
@@ -2,17 +2,17 @@ import datetime
 import io
 import os
 
+import aiocron
 import aiohttp
 import discord
 from dateutil import tz
-from discord.ext import commands, tasks
 from dotenv import load_dotenv
 
 from database import Database
 from image import Image
 
 load_dotenv()
-DEBUG = os.getenv("DEBUG", False)
+DEBUG = os.getenv("DEBUG", "False") == "True"
 TOKEN = os.getenv("TOKEN")
 MAIN_SERVER_ID = os.getenv("MAIN_SERVER_ID")
 MAIN_CHANNEL_ID = os.getenv("MAIN_CHANNEL_ID")
@@ -20,17 +20,13 @@ MAIN_USER_ID = os.getenv("MAIN_USER_ID")
 CHANNEL_IDS = os.getenv("CHANNEL_IDS")
 DIR_PATH = os.path.abspath('.')
 
-intents = discord.Intents.default()
-intents.message_content = True
-client = discord.Client(intents=intents)
-time = datetime.time(hour=9, tzinfo=tz.gettz("US/Eastern"))
+client = discord.Client()
 
 @client.event
 async def on_ready():
     print(f"Logged on as {client.user}")
     client.channels = await get_channels()
     client.database = Database(f"{DIR_PATH}/db/cat.db")
-    client.daily_pic = DailyPic()
 
 
 @client.event
@@ -80,21 +76,12 @@ async def send_image_from_db():
     for channel in client.channels:
         await channel.send(file=discord.File(f"{DIR_PATH}/images/{file_name}"))
 
+@aiocron.crontab("0 9 * * *")
+async def cron_send_pic():
+    await send_image_from_db()
 
-class DailyPic(commands.Cog):
-    def __init__(self):
-        self.cron_send_pic.start()
-
-    def cog_unload(self):
-        self.my_task.cancel()
-
-    @tasks.loop(time=time)
-    async def cron_send_pic(self):
-        await send_image_from_db()
-
-        if DEBUG:
-            channel = client.get_channel(int(MAIN_CHANNEL_ID))
-            await send_ping_response(channel)
-
+    if DEBUG:
+        channel = client.get_channel(int(MAIN_CHANNEL_ID))
+        await send_ping_response(channel)
 
 client.run(TOKEN)
